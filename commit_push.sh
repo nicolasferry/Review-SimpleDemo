f5811d0614b730b1b13e861948fa3a15e0580fed
nicolasferry <nicolas.ferry@sintef.no>
2019-08-19 22:30:59 +0200

    commit for the demo

--- commit_push.sh
+++ commit_push.sh
@@ -2,4 +2,4 @@
 
 git commit -m "commit for the demo" .
 git push
-curl -u nico:nico http://nico:nico@127.0.0.1:8080/job/Demo-Jenkins-GeneSIS/build?token=genesis
+curl -u nico:nico http://nico:nico@127.0.0.1:8080/job/Demo-Review/build?token=genesis
